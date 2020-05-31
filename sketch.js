091ae545a95ebe307ac4febc084953c75ce6f512
CodingMST <63578684+CodingMST@users.noreply.github.com>
2020-05-31 17:40:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(800,400);
+  
+}
+
+
+
+function draw() {
+  background(203,1,79);  
+  drawSprites();
+  
+  fill("blue") 
+rect(170, 85, 500, 45);
+fill("yellow")
+ellipse(403,321,100);
+fill("white") 
+rect(400, 100, 10, 200);
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
